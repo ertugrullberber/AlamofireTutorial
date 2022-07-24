db9a856980ff72012a86044ceb2eb60687213484
ertugrullberber <ertugrulberber3461@gmail.com>
2022-07-24 14:53:46 +0300

    Initial Commit

---
+++ AlamofireTutorial/AlamofireTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireTutorialApp.swift
+//  AlamofireTutorial
+//
+//  Created by Ertugrul Berber on 24.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
